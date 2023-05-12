9b93471efb28adfe203225f4e6b70d7b9c6adf59
tokaretin <tokaretin@gmail.com>
2023-05-12 11:13:11 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_cycle_DO_WHILE/SimpleCode_cycle_DO_WHILE.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int begin, end;
+    cout << "Введите диапазон чисел:\n";
+    cin >> begin >> end;
+
+    int sum = 0;
+
+    do
+    {
+
+        if (begin % 2 != 0)
+            sum += begin;
+        begin++;
+        
+    } while (begin <= end);
+
+    cout << "Сумма нечетных числел = " << sum;
+}
+
